638a8d0926461933fc50ea0a5a70e53f2fca8d0a
Jungpyo Lee <ljp0187@gmail.com>
2024-01-22 00:04:54 -0800

    Upload file of basic rtde for Alahe

---
+++ src/Alahe_basic_rtde.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+
+# Authors: Jungpyo Lee
+
+# imports
+try:
+  import rospy
+  import tf
+  ros_enabled = True
+except:
+  print('Couldn\'t import ROS.  I assume you\'re running this on your laptop')
+  ros_enabled = False
+
+from calendar import month_abbr
+from helperFunction.utils import rotation_from_quaternion, create_transform_matrix, quaternion_from_matrix, normalize, hat
+
+
+from datetime import datetime
+import pandas as pd
+import numpy as np
+from scipy.io import savemat
+from scipy.spatial.transform import Rotation as sciRot
+
+
+from netft_utils.srv import *
+from suction_cup.srv import *
+from std_msgs.msg import String
+from std_msgs.msg import Int8
+import geometry_msgs.msg
+import tf
+
+from math import pi, cos, sin
+
+from helperFunction.rtde_helper import rtdeHelp
+from helperFunction.adaptiveMotion import adaptMotionHelp
+
+
+def main(args):
+
+  np.set_printoptions(precision=4)
+
+  # controller node
+  rospy.init_node('suction_cup')
+
+  # Setup helper functions
+  rospy.sleep(0.5)
+  rtde_help = rtde_help = rtdeHelp(125)
+  rospy.sleep(0.5)
+  adpt_help = adaptMotionHelp(dw = 0.5, d_lat = 0.5e-3, d_z = 0.1e-3)
+
+  # Set the TCP offset and calibration matrix
+  rospy.sleep(0.5)
+  rtde_help.setTCPoffset([0, 0, 0.150, 0, 0, 0])
+  rospy.sleep(0.2)
+  rtde_help.setCalibrationMatrix()
+  rospy.sleep(0.2)
+
+  
+  # pose initialization
+  disengagePosition_init =  [-0.597, .211, 0.025] # unit is in m
+  setOrientation = tf.transformations.quaternion_from_euler(pi,0,pi/2,'sxyz') #static (s) rotating (r)
+  disEngagePose = rtde_help.getPoseObj(disengagePosition_init, setOrientation)
+
+
+  # try block so that we can have a keyboard exception
+  try:
+    # Go to disengage Pose
+    input("Press <Enter> to go disEngagePose")
+    rtde_help.goToPose(disEngagePose)
+    rospy.sleep(0.1)
+
+    print("============ Python UR_Interface demo complete!")
+  
+  except rospy.ROSInterruptException:
+    return
+  except KeyboardInterrupt:
+    return  
+
+
+if __name__ == '__main__':  
+  import argparse
+  parser = argparse.ArgumentParser()
+  args = parser.parse_args()    
+  
+  main(args)
