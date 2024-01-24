4b006b667105286004b5ab857aa64797cba7829d
Akhavana <156957091+Akhavana@users.noreply.github.com>
2024-01-24 11:49:43 -0800

    added distance monitor

--- src/Alahe_basic_rtde.py
+++ src/Alahe_basic_rtde.py
@@ -18,6 +18,7 @@ from helperFunction.utils import rotation_from_quaternion, create_transform_matr
 from datetime import datetime
 import pandas as pd
 import numpy as np
+import math
 from scipy.io import savemat
 from scipy.spatial.transform import Rotation as sciRot
 
@@ -28,6 +29,8 @@ from std_msgs.msg import String
 from std_msgs.msg import Int8
 import geometry_msgs.msg
 import tf
+
+# import rtde_control
 
 from math import pi, cos, sin
 
@@ -58,18 +61,41 @@ def main(args):
   
   # pose initialization
   disengagePosition_init =  [-0.597, .211, 0.025] # unit is in m
-  setOrientation = tf.transformations.quaternion_from_euler(pi,0,pi/2,'sxyz') #static (s) rotating (r)
-  disEngagePose = rtde_help.getPoseObj(disengagePosition_init, setOrientation)
+  setOrientation = tf.transformations.quaternion_from_euler(pi,0,pi/2,'sxyz') #static (s) rotating (r), the orientaiton/frame of the trajec
+  disEngagePose = rtde_help.getPoseObj(disengagePosition_init, setOrientation) 
+
+  def calculate_distance(current_position, target_position):
+    return ((current_position[0] - target_position[0])**2 +
+            (current_position[1] - target_position[1])**2 +
+            (current_position[2] - target_position[2])**2) ** 0.5
+  
+  distance = calculate_distance(disengagePosition_init, disEngagePose)
+
+  #subscribing to receive pose actively
+  # data = rtdeHelp.receive()
+  # #looking for the real time pose
+  # rPose = data.actual_tool_pose
 
 
   # try block so that we can have a keyboard exception
   try:
     # Go to disengage Pose
     input("Press <Enter> to go disEngagePose")
-    rtde_help.goToPose(disEngagePose)
+    rtde_help.goToPose(disEngagePose)  #goes to the pose, in attempt to make a second attempt creates roundoff error
+    # rtde_help.goToPoseAdaptive(disEngagePose)
     rospy.sleep(0.1)
+    #displaying the distance at each step
+    
+    while not distance <0.01: 
+      current_pose = rtde_help.getTCPPose(disEngagePose) #attempt to get updated pose as the UR10 is moving
+      print("Current pose is: ", current_pose)
+      print("Received distance is: ", calculate_distance(current_pose, disengagePosition_init))
 
+    
+    print("Calcualted distance is: ", distance)
     print("============ Python UR_Interface demo complete!")
+
+    
   
   except rospy.ROSInterruptException:
     return
